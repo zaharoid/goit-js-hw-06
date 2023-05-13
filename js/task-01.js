7cc26966a8a7a5278f64c104a3603d7bbf472046
zaharoid <gololobov.zahar23@gmail.com>
2023-05-13 23:22:14 +0200

    debagging

--- js/task-01.js
+++ js/task-01.js
@@ -5,7 +5,6 @@ console.log(`Number of categories: ${childrenOfList.length}`);
 
 const children = [...childrenOfList]
 
-
 children.forEach(item => {
     console.log(`Category: ${item.firstElementChild.textContent}`);
     console.log(`Elements: ${item.lastElementChild.children.length}`)
--- js/task-04.js
+++ js/task-04.js
@@ -1,19 +1,16 @@
 const refs = {
-    decrement: document.querySelector('[data-action="decrement"]'),
-    increment: document.querySelector('[data-action="increment"]'),
-    counterValue: document.querySelector('#value')
+  decrement: document.querySelector('[data-action="decrement"]'),
+  increment: document.querySelector('[data-action="increment"]'),
+  counterValue: document.querySelector("#value"),
+};
+
+refs.decrement.addEventListener("click", onDecrementClick);
+refs.increment.addEventListener("click", onIncrementClick);
+
+function onDecrementClick() {
+  refs.counterValue.textContent = parseInt(refs.counterValue.textContent) - 1;
 }
 
-refs.decrement.addEventListener('click', onDecrementClick)
-refs.increment.addEventListener('click', onIncrementClick)
-
-
-
-
-function onDecrementClick () {
-    refs.counterValue.textContent = parseInt(refs.counterValue.textContent) - 1
-}
-
-function onIncrementClick () {
-    refs.counterValue.textContent = parseInt(refs.counterValue.textContent) + 1
+function onIncrementClick() {
+  refs.counterValue.textContent = parseInt(refs.counterValue.textContent) + 1;
 }
